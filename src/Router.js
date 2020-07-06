6e8abad4f1c776e64ce1be3d8e94cd92562a7864
solucaocriativaoficial <solucaocriativaoficial@gmail.com>
2020-07-05 23:41:07 -0400

    add middleware access, signin, gestão of user

--- src/Router.js
+++ src/Router.js
@@ -4,30 +4,40 @@ const CdController = require('./controllers/CdController');
 const SongController = require('./controllers/SongController');
 const LetterController = require('./controllers/LetterController');
 const DateControllerSync = require('./controllers/dateController');
+const UserController = require('./controllers/UserController');
+const Auth = require('./middleware/Auth');
+const SignIn = require('./controllers/SignIn');
 
 const Router = express();
 
 Router.get('/cd/list/', CdController.findAll);
-Router.post('/cd/create/', CdController.insert);
-Router.put('/cd/update/:id', CdController.update);
-Router.delete('/cd/delete/:id', CdController.delete);
-
-Router.get('/song/list/', SongController.findAll);
 Router.get('/song/cd/:cd_id', SongController.findForCd);
-Router.post('/song/create/', SongController.insert);
-Router.put('/song/update/:id', SongController.update);
-Router.delete('/song/delete/:id', SongController.delete);
-
-Router.get('/letter/list/', LetterController.findForSync);
 Router.get('/letter/list/:song_id', LetterController.findAll);
 Router.get('/letter/list/1/:id', LetterController.findId);
-Router.post('/letter/create/', LetterController.insert);
-Router.put('/letter/update/:id', LetterController.update);
-Router.delete('/letter/delete/:id', LetterController.delete);
-
 
 Router.post('/sync/cd/', DateControllerSync.searchDateCd)
 Router.post('/sync/song/', DateControllerSync.searchDateSong)
 Router.post('/sync/letter/', DateControllerSync.searchDateLetter)
+
+Router.post('/signin', SignIn.signIn)
+
+Router.use(Auth);
+
+Router.get('/user/list/', UserController.findAll);
+Router.post('/user/create/', UserController.insert);
+Router.put('/user/update/:id', UserController.update);
+Router.delete('/user/delete/:id', UserController.delete);
+
+Router.post('/cd/create/', CdController.insert);
+Router.put('/cd/update/:id', CdController.update);
+Router.delete('/cd/delete/:id', CdController.delete);
+
+Router.post('/song/create/', SongController.insert);
+Router.put('/song/update/:id', SongController.update);
+Router.delete('/song/delete/:id', SongController.delete);
+
+Router.post('/letter/create/', LetterController.insert);
+Router.put('/letter/update/:id', LetterController.update);
+Router.delete('/letter/delete/:id', LetterController.delete);
 
 module.exports = Router;
