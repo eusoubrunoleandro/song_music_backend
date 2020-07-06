8e4b1f8fc5206121e7845966a90fc22578390c1f
solucaocriativaoficial <solucaocriativaoficial@gmail.com>
2020-07-05 23:49:12 -0400

    verify if error is in token signin

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -4,13 +4,15 @@ function checkToken(req, res, next){
     const token = req.headers.auth;
     if(token === undefined || token === '')
     res.status(401).json({
-        message: 'Precisamos que você faça login novamente ;)'
+        message: 'Precisamos que você faça login novamente ;)',
+        signin: false,
     })
 
     jwt.verify(token, process.env.SECRET_KEY_TOKEN, (err, decoded) => {
         if(err)
         res.status(401).json({
-            message: 'Sua sessão expirou, faça login novamente ;)'
+            message: 'Sua sessão expirou, faça login novamente ;)',
+            signin: false,
         })
 
         const {usot} = decoded;
