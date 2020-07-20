08c1a58f77301bac0d35da1cb28eaecefe62668b
solucaocriativaoficial <solucaocriativaoficial@gmail.com>
2020-07-19 23:07:48 -0400

    cors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const Router = require('./Router');
 const app = Express();
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next()
 })
 app.use(bodyParser.json());
