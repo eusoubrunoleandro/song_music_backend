e793c905ecdfcf30fb71496e75e98601dd44c310
solucaocriativaoficial <solucaocriativaoficial@gmail.com>
2020-07-19 22:36:26 -0400

    cors for options of requisition

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ const app = Express();
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Origin", "GET,PUT,POST,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Origin", "Content-Type");
-
     next()
 })
 app.use(bodyParser.json());
