2bdbc13699f8cb3a5de6e1c41d782aaf2a8f4d42
solucaocriativaoficial <solucaocriativaoficial@gmail.com>
2020-07-26 10:23:55 -0400

    corrigindo acesso

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 const Express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 
 const Router = require('./Router');
 const app = Express();
 
-app.use(cors())
-app.use(bodyParser.json());
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
 app.use(Router)
 
 app.listen(process.env.PORT || 3333)
